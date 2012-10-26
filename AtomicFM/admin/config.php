308e97f51bbbf58efd94b2f417e4420ddd6281b4
xweideveloper <weix1983@gmail.com>
2012-10-26 14:56:42 +1000

    OpenCart Practice
    I have to build a eCommerce Website Demo using OpenCart. It is required
    by Atomic Fasion Marketing Campany

---
+++ AtomicFM/admin/config.php
@@ -0,0 +1,32 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://127.0.0.1/AtomicFM/admin/');
+define('HTTP_CATALOG', 'http://127.0.0.1/AtomicFM/');
+define('HTTP_IMAGE', 'http://127.0.0.1/AtomicFM/image/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://127.0.0.1/AtomicFM/admin/');
+define('HTTPS_CATALOG', 'http://127.0.0.1/AtomicFM/');
+define('HTTPS_IMAGE', 'http://127.0.0.1/AtomicFM/image/');
+
+// DIR
+define('DIR_APPLICATION', '/Applications/AMPPS/www/AtomicFM/admin/');
+define('DIR_SYSTEM', '/Applications/AMPPS/www/AtomicFM/system/');
+define('DIR_DATABASE', '/Applications/AMPPS/www/AtomicFM/system/database/');
+define('DIR_LANGUAGE', '/Applications/AMPPS/www/AtomicFM/admin/language/');
+define('DIR_TEMPLATE', '/Applications/AMPPS/www/AtomicFM/admin/view/template/');
+define('DIR_CONFIG', '/Applications/AMPPS/www/AtomicFM/system/config/');
+define('DIR_IMAGE', '/Applications/AMPPS/www/AtomicFM/image/');
+define('DIR_CACHE', '/Applications/AMPPS/www/AtomicFM/system/cache/');
+define('DIR_DOWNLOAD', '/Applications/AMPPS/www/AtomicFM/download/');
+define('DIR_LOGS', '/Applications/AMPPS/www/AtomicFM/system/logs/');
+define('DIR_CATALOG', '/Applications/AMPPS/www/AtomicFM/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'xwei');
+define('DB_PASSWORD', 'xwei1983');
+define('DB_DATABASE', 'atomicdb');
+define('DB_PREFIX', '');
+?>
---
+++ AtomicFM/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://127.0.0.1/AtomicFM/');
+define('HTTP_IMAGE', 'http://127.0.0.1/AtomicFM/image/');
+define('HTTP_ADMIN', 'http://127.0.0.1/AtomicFM/admin/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://127.0.0.1/AtomicFM/');
+define('HTTPS_IMAGE', 'http://127.0.0.1/AtomicFM/image/');
+
+// DIR
+define('DIR_APPLICATION', '/Applications/AMPPS/www/AtomicFM/catalog/');
+define('DIR_SYSTEM', '/Applications/AMPPS/www/AtomicFM/system/');
+define('DIR_DATABASE', '/Applications/AMPPS/www/AtomicFM/system/database/');
+define('DIR_LANGUAGE', '/Applications/AMPPS/www/AtomicFM/catalog/language/');
+define('DIR_TEMPLATE', '/Applications/AMPPS/www/AtomicFM/catalog/view/theme/');
+define('DIR_CONFIG', '/Applications/AMPPS/www/AtomicFM/system/config/');
+define('DIR_IMAGE', '/Applications/AMPPS/www/AtomicFM/image/');
+define('DIR_CACHE', '/Applications/AMPPS/www/AtomicFM/system/cache/');
+define('DIR_DOWNLOAD', '/Applications/AMPPS/www/AtomicFM/download/');
+define('DIR_LOGS', '/Applications/AMPPS/www/AtomicFM/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'xwei');
+define('DB_PASSWORD', 'xwei1983');
+define('DB_DATABASE', 'atomicdb');
+define('DB_PREFIX', '');
+?>
